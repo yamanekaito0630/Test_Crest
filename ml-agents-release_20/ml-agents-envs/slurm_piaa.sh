3f5867024d7a94d9e617d96d61b8c761dddae49e
yamanekaito0630 <yamanekaito0630@gmail.com>
2023-12-25 09:30:33 +0900

    edit

--- ml-agents-release_20/ml-agents-envs/slurm_piaa.sh
+++ ml-agents-release_20/ml-agents-envs/slurm_piaa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -J 20robo
+#SBATCH -J 1robo
 #SBATCH -p matisse
-#SBATCH -N 4
+#SBATCH -N 2
 #SBATCH -n 32
 #SBATCH -o stdout.%J.log
 #SBATCH -e stderr.%J.log
 
 source ../../env/bin/activate
-mpirun -n 32 --oversubscribe python train_mpi_evojax.py --log-dir=log/at4/round_im_20_robo_slurm_at4 --max-iter=1000 --base=piaa --n-robo=20 --n-at=4 --version=1 --n-fitness=6
+mpirun -n 32 --oversubscribe python train_mpi_evojax.py --log-dir=log/at8/round_im_1_robo_slurm_at8 --max-iter=500 --base=piaa --n-robo=1 --n-at=8 --version=1 --n-fitness=7
 deactivate
 
