04237d2d1c21ef92d09cc16e2fc851530fc67a8f
yamanekaito0630 <yamanekaito0630@gmail.com>
2024-02-22 16:31:21 +0900

    edit

--- ml-agents-release_20/ml-agents-envs/slurm_piaa.sh
+++ ml-agents-release_20/ml-agents-envs/slurm_piaa.sh
@@ -7,6 +7,6 @@
 #SBATCH -e stderr.%J.log
 
 source ../../env/bin/activate
-mpirun -n 32 --oversubscribe python train_mpi_evojax.py --max-iter=1000 --base=piaa --ns-robo 4 8 --n-at=9 --version=1 --n-fitness=7 --ns-attempt 1 2 3
+mpirun -n 32 --oversubscribe python train_mpi_evojax.py --max-iter=1000 --base=piaa --ns-robo 4 8 --n-at=9 --version=1 --n-fitness=7 --ns-trial 1 2 3
 deactivate
 
